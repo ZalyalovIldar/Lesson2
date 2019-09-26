956d5115fd616e0ed43a8cfb407e23b7e18ec42c
Nikita <luzhbinnikita@gmail.com>
2019-09-26 11:23:08 +0300

    Initial

---
+++ vkPage/vkPage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vkPage
+//
+//  Created by Никита Лужбин on 26/09/2019.
+//  Copyright © 2019 Никита. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
