1443565e1becd25c0a97011e2c597c10189994f5
Nikita <luzhbinnikita@gmail.com>
2019-09-26 18:53:03 +0300

    [Add] Main Design

--- vkPage/vkPage/ViewController.swift
+++ vkPage/vkPage/ViewController.swift
@@ -1,18 +1,15 @@
-//
-//  ViewController.swift
-//  vkPage
-//
-//  Created by Никита Лужбин on 26/09/2019.
-//  Copyright © 2019 Никита. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userPhoto: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        userPhoto.layer.cornerRadius = userPhoto.frame.height / 2
+        userPhoto.clipsToBounds = true
+        
     }
 
 
