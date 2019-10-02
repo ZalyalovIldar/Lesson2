1011fdd4f5b539a5b97680c8fbbc34b7bb11d935
Emil <42997578+emilmubarakov@users.noreply.github.com>
2019-10-02 15:50:36 +0300

    Trying to add a million number...

---
+++ Homework/VKHomework/VKHomework/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VK Homework
+//
+//  Created by Emil Mubarakov on 01/10/2019.
+//  Copyright © 2019 Emil Mubarakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    
+    }
+
+}
+
